c4b30512b6879068c3f36590115f0d4a9373205a
gabriel-le-friedrich <gabriellefriedrich04@gmail.com>
2026-09-17 22:46:24 +0800

    Update STUN and TURN server configurations

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ const peerConfig = {
     iceServers: [
       { urls: "stun:stun.l.google.com:19302" },
       { urls: "stun:stun1.l.google.com:19302" },
-      { urls: "stun:global.stun.twilio.com:3478" },
+      { urls: "stun:stun.metered.ca:19302" },
       {
-        urls: "turn:openrelay.metered.ca:80",
-        username: "openrelayproject",
-        credential: "openrelayproject"
+        urls: "turn:a.relay.metered.ca:80",
+        username: "e8ca92e44f8ca8f5da00ddcc",
+        credential: "6q+mN6H8V/9sXmGj"
       },
       {
-        urls: "turn:openrelay.metered.ca:443",
-        username: "openrelayproject",
-        credential: "openrelayproject"
+        urls: "turn:a.relay.metered.ca:443",
+        username: "e8ca92e44f8ca8f5da00ddcc",
+        credential: "6q+mN6H8V/9sXmGj"
+      },
+      {
+        urls: "turn:a.relay.metered.ca:443?transport=tcp",
+        username: "e8ca92e44f8ca8f5da00ddcc",
+        credential: "6q+mN6H8V/9sXmGj"
       }
     ]
   }
